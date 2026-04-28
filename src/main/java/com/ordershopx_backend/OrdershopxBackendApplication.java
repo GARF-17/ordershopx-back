79fe12c3333a398ae6fe713172bea1065b66d448
GARF-17 <garf2117@gmail.com>
2026-04-28 01:21:45 -0500

    feat: configuracion inicial del proyecto

---
+++ src/main/java/com/ordershopx_backend/OrdershopxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ordershopx_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdershopxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdershopxBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ordershopx_backend/OrdershopxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordershopx_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdershopxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
